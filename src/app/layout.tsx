0b801f01dbf4923b95bad83b5ec2c4f11ccb74d0
Treelar <me@treelar.xyz>
2025-04-28 19:42:55 -0700

    specify font features for inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,21 @@
 // import "~/styles/globals.css";
 
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import { headers } from "next/headers";
 import "../styles/globals.css";
 
 import { TRPCReactProvider } from "~/utils/trpc/react";
 import { SessionProvider } from "~/utils/auth/sessionProvider";
 
-const inter = Inter({
-  subsets: ["latin"],
+// const inter = Inter({
+//   subsets: ["latin"],
+//   variable: "--font-sans",
+// });
+
+const inter = localFont({
+  src: "../../assets/InterVariable.ttf",
   variable: "--font-sans",
+  display: "swap",
 });
 
 export default async function RootLayout({
@@ -19,7 +25,7 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>
+      <body className={`font-sans ${inter.variable}`} style={{fontFeatureSettings: `"tnum", "ss01", "ss04", "calt", "liga"`}}>
         <SessionProvider>
           <TRPCReactProvider headers={await headers()}>
             <main className="hero min-h-screen bg-base-300">
