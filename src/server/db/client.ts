253aa3f6a040f40b0f621d76afa25fce5ef4307e
ninjawarrior1337 <me@treelar.xyz>
2025-12-03 13:11:17 -0800

    upgrade to prisma 7

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,5 +1,6 @@
 // src/server/db/client.ts
 import { PrismaClient } from "~/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { env } from "../../env/server.mjs";
 
 declare global {
@@ -7,10 +8,15 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL 
+})
+
 export const prisma =
   global.prisma ||
   new PrismaClient({
     log: ["query"],
+    adapter
   });
 
 if (env.NODE_ENV !== "production") {
